4bc7490430f4194ee789d35b2a39328bde9de7d6
ashourml <164765699+ashourml@users.noreply.github.com>
2024-05-30 18:27:49 +0300

    update , fix model creation

--- Model_Create/model_create.py
+++ Model_Create/model_create.py
@@ -1,7 +1,7 @@
 import ollama
 
 # The code snippet you provided is creating a new model using the `ollama` library in Python.
-modelfile='''
+llava_modelfile='''
 FROM llava:latest
 SYSTEM you are expert in generating stable diffusion ai prompt respnse with just prompt
 '''
@@ -10,5 +10,14 @@ SYSTEM you are expert in generating stable diffusion ai prompt respnse with just
 # with the name 'pocket_llava' using the `ollama` library in Python. The model is being initialized
 # with the content specified in the `modelfile` variable, which in this case contains the Dockerfile
 # instructions for the model.
-ollama.create(model='pocket_llava', modelfile=modelfile)
+ollama.create(model='pocket_llava', modelfile=llava_modelfile)
+
+
+
+llama2_modelfile='''
+FROM llama2:latest
+SYSTEM you are expert in generating ai prompt , your name is pocket-prompt to make perfect output based on user input description like (modern villa design with urban style) and replay with just (1) prompt like this template :Design a modern living room with a cityscape view: Create a luxurious living room that seamlessly blends into the urban landscape outside. Use large windows to showcase the city skyline, and incorporate industrial metal accents such as exposed ductwork or metal beams to give the space an edgy feel. Add wood paneling on the walls to bring in a natural element, and use glass coffee tables to create a sleek, modern look
+'''
+
+ollama.create(model='prompt', modelfile=llama2_modelfile)
 
